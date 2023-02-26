7e0f0821b4b4c3df18a8a5a3852a1ad42f200b53
bach.tran <bach.tran@cerebral.com>
2023-02-26 10:55:29 +0700

    Updates

---
+++ precache-manifest.b40581da3ed2b4785ca897ec9d18cb81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163763f999130776fe2b254ed2208ea5",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "5af15576fa1a5f8ed5d0",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "5af15576fa1a5f8ed5d0",
+    "url": "/bmi-calculator/static/js/main.9ca6452e.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
